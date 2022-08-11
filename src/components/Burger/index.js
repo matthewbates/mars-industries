6086b64130078e0bed076285e7d6a18a724cdc95
dylandupasquier <dylandupasquier@gmail.com>
2022-08-11 13:34:42 -0700

    hamburger z index fix

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import { BurgerContainer } from "./BurgerElements";
+import React from 'react';
+import { BurgerContainer } from './BurgerElements';
 
 export default function Burger({ open, handleOnClick }) {
   return (
     <BurgerContainer
       open={open}
       onClick={handleOnClick}
-      style={{ position: "fixed", top: "6rem", right: "25px" }}
+      style={{ position: 'absolute', top: '6rem', right: '25px' }}
     >
       <div />
       <div />
