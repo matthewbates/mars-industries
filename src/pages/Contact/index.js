214e19403911533b333e18c638cc0c679dde32bb
Matthew <matthewhcbates@gmail.com>
2022-08-29 14:32:40 -0600

    home page rendering text/info correctly; contact link working; logo routes to home

---
+++ src/pages/Contact/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HeaderInfo from "../../components/HeaderInfo";
+import HeaderLinks from "../../components/HeaderNav";
+
+export default function Contact() {
+  return (
+    <div>
+      <HeaderInfo />
+      <HeaderLinks />
+      <p>This is the contact route</p>
+    </div>
+  );
+}
