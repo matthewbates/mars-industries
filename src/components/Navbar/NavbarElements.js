1172998d9ce0f8ca4fe1437d2c8010a14af330bd
Matthew <matthewhcbates@gmail.com>
2022-09-05 21:28:13 -0600

    Navbar component looking so much better

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -1,14 +1,56 @@
 import styled from "styled-components";
-import { Link as LinkR } from "react-router-dom";
+import CONSTANTS from "../../utils/constants";
+import { Link as LinkRoute } from "react-router-dom";
 
-export const NavbarContainer = styled.div`
+export const Nav = styled.nav`
+  height: 80px;
   display: flex;
-  height: 50px;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  z-index: 10;
+  background: ${CONSTANTS.colors.offWhite};
+
+  @media screen and (max-width: 960px) {
+    transition: 0.8s all ease;
+  }
 `;
 
-export const LinkBtn = styled(LinkR)`
+export const NavbarContainer = styled.div`
+  justify-content: center;
   display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  height: 80px;
+  z-index: 1;
+  width: 100%;
+  padding: 0 24px;
+  max-width: 1100px;
+`;
+
+export const NavMenu = styled.ul`
+  display: flex;
   align-items: center;
+  list-style: none;
+  text-align: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
+
+export const NavItem = styled.li`
+  height: 80px;
+`;
+
+export const NavLinks = styled(LinkRoute)`
+  color: black;
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  padding: 0 1rem;
+  height: 100%;
+  cursor: pointer;
+
+  &:hover {
+    color: orange;
+  }
 `;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,14 +1,32 @@
 import React from "react";
-import { NavbarContainer, LinkBtn } from "./NavbarElements";
+import {
+  Nav,
+  NavbarContainer,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./NavbarElements";
 import { Link } from "react-router-dom";
 import Burger from "../Burger";
+import Dropdown from "../Dropdown";
 
-export default function Navbar() {
+export default function Navbar({ isOpen, toggleDropdown }) {
   return (
-    <NavbarContainer>
-      <LinkBtn to="/services">Services</LinkBtn>
-      <LinkBtn to="/contact">Contact</LinkBtn>
-      <Burger />
-    </NavbarContainer>
+    <Nav>
+      <NavbarContainer>
+        <NavMenu>
+          <NavItem>
+            <NavLinks to="/">Home</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/services">Services</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="/contact">Contact</NavLinks>
+          </NavItem>
+        </NavMenu>
+        <Burger isOpen={isOpen} toggleDropdown={toggleDropdown} />
+      </NavbarContainer>
+    </Nav>
   );
 }
