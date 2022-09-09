b7ca403a9b09a947e3dd1c96fc26021af8574da6
Matthew <matthewhcbates@gmail.com>
2022-09-09 14:38:48 -0600

    contact form looking great

--- src/components/Form/FormElements.js
+++ src/components/Form/FormElements.js
@@ -1,2 +1,97 @@
 import styled from "styled-components";
+import CONSTANTS from "../../utils/constants";
 
+export const FormContainer = styled.div`
+  line-height: 50px;
+  padding: 2rem;
+  height: 80vh;
+  margin-top: 3rem;
+`;
+
+export const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .required::after {
+    content: "*";
+    color: red;
+  }
+`;
+
+export const FormText = styled.div``;
+
+export const TextInput = styled.input`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  padding: 15px;
+  margin-bottom: 10px;
+  font-size: 14px;
+  background-color: transparent;
+  resize: none;
+
+  @media screen and (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+export const EmailInput = styled.input`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  padding: 15px;
+  margin-bottom: 10px;
+  font-size: 14px;
+  background-color: transparent;
+  resize: none;
+
+  @media screen and (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+export const TextArea = styled.textarea`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+  padding: 15px;
+  margin-bottom: 10px;
+  font-size: 14px;
+  background-color: transparent;
+  resize: none;
+
+  @media screen and (max-width: 768px) {
+    width: 80%;
+  }
+`;
+
+export const FormLabel = styled.label`
+  display: flex;
+  line-height: 2;
+  font-family: "Open Sans";
+  /* font-size: 14px; */
+`;
+
+export const FormButton = styled.button`
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 50%;
+  cursor: pointer;
+  text-transform: uppercase;
+  padding: 20px;
+  font-weight: 900;
+  font-size: 14px;
+  letter-spacing: 5px;
+  transition: transform 0.25s;
+  border: none;
+
+  &:hover {
+    box-shadow: none;
+    transform: scale(1.02);
+  }
+`;
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,17 +1,36 @@
 import React from "react";
+import {
+  FormContainer,
+  FormWrapper,
+  UserMessage,
+  FormLabel,
+  TextInput,
+  EmailInput,
+  TextArea,
+  FormText,
+  FormButton,
+} from "./FormElements";
 
 export default function Form() {
   return (
-    <div>
-      <form>
-        <label>Name</label>
-        <input></input>
-        <label>Email</label>
-        <input></input>
-        <label>Message</label>
-        <input></input>
-        <button>Submit</button>
-      </form>
-    </div>
+    <FormContainer>
+      <FormWrapper id="myForm">
+        <FormLabel className="required">First Name:</FormLabel>
+        <TextInput type="text" name="user_first_name" required />
+        <FormLabel className="required">Last Name:</FormLabel>
+        <TextInput type="text" name="user_last_name" required />
+        <FormLabel className="required"> Email Address:</FormLabel>
+        <EmailInput type="email" name="user_email" required />
+        <FormLabel> Phone Number:</FormLabel>
+        {/* consider adding a dropdown for problems XYZ the user is inquiring about */}
+        <EmailInput type="email" name="user_email" required />
+        <FormLabel> Comments or Questions:</FormLabel>
+        <TextArea name="user_message" cols="50" rows="10" required></TextArea>
+
+        <FormButton type="submit" className="submit-button" primary="true">
+          SEND
+        </FormButton>
+      </FormWrapper>
+    </FormContainer>
   );
 }
