695d33f84342f4f944a26cb815e897e294657604
Matthew <matthewhcbates@gmail.com>
2022-09-09 11:54:46 -0600

    working on Form component

---
+++ src/components/Form/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div>
+      <form>
+        <label>Name</label>
+        <input></input>
+        <label>Email</label>
+        <input></input>
+        <label>Message</label>
+        <input></input>
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+}
