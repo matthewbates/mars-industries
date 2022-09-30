dfc86620840a064a5b1a0dd0d9121fdf297f9c32
Matthew <matthewhcbates@gmail.com>
2022-09-30 16:35:20 -0600

    added missing semicolon for netlify build

--- src/components/WhoWeAre/data.js
+++ src/components/WhoWeAre/data.js
@@ -1,6 +1,6 @@
 export const whoWeAreData = [
   {
-    text: "With over 20 years of total industry experience, Nick  knew it was time to take the leap and establish his own company. Mars Construction has steadily grown from a modest home remodeling contractor into a large-scale commerical and residential construction business. Today, the company continues to maintain their client-first approach, and insist on bringing integrity, honesty, and transparency to every job, every time.",
+    text: "Mars Construction was founded by Nick Garcia in 2021 who has steadily been growing his reputation ever since as one of Colorado Springs' premier general contractors. has steadily grown from a modest home remodeling contractor into a large-scale commerical and residential construction business. Today, the company continues to maintain their client-first approach, and insist on bringing integrity, honesty, and transparency to every job, every time.",
   },
   {
     text: "Our continued success is due in part to our commitment practicing strict compliance with all of Colorado's laws, rules, and regulations. Exercising safe construction methods is always a top priority of ours. We share a commonality with all clients: to deliver a beautiful end product.",
