1ccd5c65f68fada4de909b92d0c2705138ba76db
Matthew <matthewhcbates@gmail.com>
2022-10-01 18:36:28 -0600

    fixed number

--- src/components/infoData.js
+++ src/components/infoData.js
@@ -3,8 +3,8 @@ import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 export const infoData = [
   {
     title: "Phone",
-    href: "tel:+7193132700",
-    contact: "(719) 313-2700)",
+    href: "tel:+17193132700",
+    contact: "(719) 313-2700",
     icon: faPhone,
   },
   {
